c8e241585254d337dbe9867df373427c8e1f7a7f
Henok R. Bedassa <jackbalageru@users.noreply.github.com>
2022-05-20 10:47:41 +0300

    Update Navbar.js

--- client/src/components/common/Navbar.js
+++ client/src/components/common/Navbar.js
@@ -3,51 +3,64 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
 	return (
-		<nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
+		<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
 			<div className="container">
-				<ul className="navbar-nav mr-auto">
-					<NavLink className="navbar-brand" hrefLang="https://henok.us" to="/">
-						Henok
-					</NavLink>
-					<li className="nav-item">
-						<NavLink className="nav-link" activeClassName="active" to="/">
-							Home
-						</NavLink>
-					</li>
-
-					<li className="nav-item">
-						<NavLink className="nav-link" activeClassName="active" to="/cruds">
-							CRUDs
-						</NavLink>
-					</li>
-					<li className="nav-item">
-						<NavLink
-							className="nav-link"
-							activeClassName="active"
-							to="/cruds/new"
-						>
-							Create
-						</NavLink>
-					</li>
-					<li className="nav-item">
-						<NavLink
-							className="nav-link"
-							activeClassName="active"
-							to="/cruds/grid-view"
-						>
-							Grid View
-						</NavLink>
-					</li>
-					<li className="nav-item">
-						<NavLink
-							className="nav-link"
-							activeClassName="active"
-							to="/cruds/list-view"
-						>
-							List View
-						</NavLink>
-					</li>
-				</ul>
+				<NavLink className="navbar-brand" hrefLang="https://henok.us" to="/">
+					Henok
+				</NavLink>
+				<button
+					className="navbar-toggler collapsed"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#mobileMenu"
+					aria-controls="mobileMenu"
+					aria-expanded="false"
+					aria-label="Toggle navigation"
+				>
+					<span className="toggler-icon top-bar"></span>
+					<span className="toggler-icon middle-bar"></span>
+					<span className="toggler-icon bottom-bar"></span>
+				</button>
+				<div className="collapse navbar-collapse" id="mobileMenu">
+					<ul className="navbar-nav me-auto mb-2 mb-lg-0">
+						<li className="nav-item">
+							<NavLink
+								className="nav-link"
+								activeClassName="active"
+								to="/cruds/new"
+							>
+								Create
+							</NavLink>
+						</li>
+						<li className="nav-item">
+							<NavLink
+								className="nav-link"
+								activeClassName="active"
+								to="/cruds"
+							>
+								CRUDs
+							</NavLink>
+						</li>
+						<li className="nav-item">
+							<NavLink
+								className="nav-link"
+								activeClassName="active"
+								to="/cruds/grid-view"
+							>
+								Grid View
+							</NavLink>
+						</li>
+						<li className="nav-item">
+							<NavLink
+								className="nav-link"
+								activeClassName="active"
+								to="/cruds/list-view"
+							>
+								List View
+							</NavLink>
+						</li>
+					</ul>
+				</div>
 			</div>
 		</nav>
 	);
